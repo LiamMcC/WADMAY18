e3103292d66c9cb14400663aff8e6e7d7f34d3d0
liamme <lmccabe@staff.ncirl.ie>
2018-07-19 12:29:41 +0000

    Read Commit

--- app.js
+++ app.js
@@ -3,6 +3,35 @@ var app = express();
 const path = require('path');
 const VIEWS = path.join(__dirname, 'views');
 
+var mysql = require('mysql'); // bring the middleware in to the application  
+
+app.set("view engine", "jade"); // This line sets the default view wngine 
+// Establish a database connection
+
+const db = mysql.createConnection({
+
+  host     : 'hostingmysql304.webapps.net',
+  user     : 'liamme',
+  password : 'L1Am39??',
+ database:'liam'
+
+});
+
+db.connect((err) => {
+  if(err){
+    console.log("You Broke it...")
+   // throw(err)  // Un comment this to throw the error rather than just log "You broke it"
+  }
+  else{
+  
+  console.log("Getting there liam it connected...")
+  }
+});
+
+
+// End establish connection
+
+
 
 app.use(express.static("scripts")); // allow the application to access the scripts folder contents to use in the application
 app.use(express.static("images")); // allow the application to access the images folder contents to use in the application
@@ -11,18 +40,50 @@ app.use(express.static("images")); // allow the application to access the images
 
 
 // function to render the home page
-app.get('/', function(req, res){
- // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
-  res.render('index.jade', {root: VIEWS});
-  console.log("Now you are home!");
-});
-
-// function to render the products page
 app.get('/products', function(req, res){
- // res.send("Hello cruel world!"); // This is commented out to allow the index view to be rendered
-  res.render('products.jade', {root: VIEWS}); // use the render command so that the response object renders a HHTML page
+ let sql = 'SELECT * FROM products'
+  let query = db.query(sql, (err, res1) => {
+    if(err) throw err;
+    console.log(res1);
+
+    res.render('products.jade', {root: VIEWS,res1});
+  });
+ 
   console.log("Now you are on the products page!");
 });
+
+
+
+
+// SQL create table Example
+app.get('/createtable', function(req, res) {
+  let sql = 'CREATE TABLE products ( Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, Name varchar(255), Price int, Image varchar(255),Activity varchar(255));'
+  let query = db.query(sql, (err, res) => {
+    if(err) throw err;
+    console.log(res);
+    
+    
+  });
+  res.send("Well done liam...");
+  });
+
+// End SQL create table Example
+
+
+// SQL Insert Example
+app.get('/insert', function(req, res) {
+  let sql = 'INSERT INTO products (Name, Price, Image, Activity) VALUES ("Polar M400", 199, "polarm400.png", "Running")'
+  let query = db.query(sql, (err, res) => {
+    if(err) throw err;
+    console.log(res);
+    
+    
+  });
+  res.send("Item Created and inserted to table...");
+  });
+
+// End SQL Insert Example
+
 
 
 
@@ -32,4 +93,16 @@ app.get('/products', function(req, res){
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" , function(){
   console.log("App is Running ......... Yessssssssssssss!")
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
